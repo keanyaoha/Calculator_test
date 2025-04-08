514f7b9109d05437e94e929834d2dbb5ba48742c
Ghazal Moradi <ghazal.moradi888@gmail.com>
2025-04-08 23:35:38 +0200

    Update Chatbot.py

--- pages/Chatbot.py
+++ pages/Chatbot.py
@@ -1,4 +1,38 @@
-%%writefile rag_app.py
+
+
+# --- App Config ---
+st.set_page_config(
+    page_title="Green Tomorrow",
+    page_icon="🌿",
+    layout="centered"
+)
+
+# --- Force Logo to Appear at Top of Sidebar ---
+st.markdown(
+    """
+    <style>
+        [data-testid="stSidebar"]::before {
+            content: "";
+            display: block;
+            background-image: url('https://raw.githubusercontent.com/GhazalMoradi8/Carbon_Footprint_Calculator/main/GreenPrint_logo.png');
+            background-size: 90% auto;
+            background-repeat: no-repeat;
+            background-position: center;
+            height: 140px;
+            margin: 1.5rem auto -4rem auto;  /* SUPER tight top & bottom spacing */
+        }
+
+        section[data-testid="stSidebar"] {
+            background-color: #d6f5ec;
+        }
+
+        .stApp {
+            background-color: white;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
