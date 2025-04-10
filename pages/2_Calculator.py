c615fb91ca83aef12577d0cdd7f06dcf683b3518
Ghazal Moradi <ghazal.moradi888@gmail.com>
2025-04-11 01:05:40 +0200

    Update 2_Calculator.py

--- pages/2_Calculator.py
+++ pages/2_Calculator.py
@@ -300,7 +300,6 @@ if st.session_state.selected_country != "-- Select --":
                     # Simplified styling - COMMENTED OUT advanced styling
                     fig_comp.update_traces(
                          texttemplate='%{text:.1f}', textposition='outside',
-                         bargap=0.1,
                          hovertemplate="<b>%{y}</b>: %{x:.1f} kg CO₂<extra></extra>"
                     )
                     fig_comp.update_layout(
