0b4f8e67287ab630c64dbfbeb4c869b6e06c80c0
Ghazal Moradi <ghazal.moradi888@gmail.com>
2025-04-11 01:18:51 +0200

    Update 2_Calculator.py

--- pages/2_Calculator.py
+++ pages/2_Calculator.py
@@ -306,7 +306,8 @@ if st.session_state.selected_country != "-- Select --":
                     fig_comp.update_layout(
                          yaxis={'categoryorder':'total ascending'},
                          bargap=0.3,
-                         height=200, 
+                         height=350,
+                         width=500,
                          margin=dict(l=5, r=5, t=50, b=20), showlegend=False
                     )
                     st.plotly_chart(fig_comp, use_container_width=True)
